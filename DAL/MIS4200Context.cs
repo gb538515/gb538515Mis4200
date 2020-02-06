aff9f59fa5b43eb914bbbe757af2de3819379fe1
Garrett Beaverson <gb538515@ohio.edu>
2020-02-05 20:01:10 -0500

    Finished In Class

---
+++ DAL/MIS4200Context.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using gb538515Mis4200.Models; // This is needed to access the models
+using System.Data.Entity; // this is needed to access the DbContext object
+
+namespace gb538515Mis4200.DAL
+{
+    public class MIS4200Context : DbContext // inherits from DbContext
+    {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<MIS4200Context,
+gb538515Mis4200.Migrations.MISContext.Configuration>("DefaultConnection"));
+            // this method is a 'constructor' and is called when a new context is created
+            // the base attribute says which connection string to use
+        }
+        // Include each object here. The value inside <> is the name of the class,
+        // the value outside should generally be the plural of the class name
+        // and is the name used to reference the entity in code
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Products> Products { get; set; }
+        public DbSet<orderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace gb538515Mis4200.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+        public DateTime customerSince { get; set; }
+
+        public ICollection<Order> Order { get; set; }
+    }
+}
